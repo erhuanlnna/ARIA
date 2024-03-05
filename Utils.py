4dac5a37e6900bf111023aec6e1335f86509f52a
huanhuan <43327770+erhuanlnna@users.noreply.github.com>
2024-03-05 15:32:45 +0800

    Add files via upload

---
+++ Utils.py
@@ -0,0 +1,155 @@
+import mysql.connector
+from dbSettings import *
+
+
+
+def connect(host = host, user=user, password=password, database=database):
+    # print(host, user, password, database)
+    conn = mysql.connector.connect(host = host, user=user, passwd=password, database=database)
+    return conn
+
+
+
+def select(sql, host = host, user=user, password=password, database=database):
+    conn = connect(host, user, password, database)
+    cursor = conn.cursor(buffered=True)
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return result
+    
+
+def insert(sql, host = host, user=user, password=password, database=database):
+    conn = connect(host, user, password, database)
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+    cursor.close()
+    conn.close()
+    
+def insert_value(sql, value, host = host, user=user, password=password, database=database):
+    conn = connect(host, user, password, database)
+    cursor = conn.cursor()
+    cursor.execute(sql, value)
+    conn.commit()
+    cursor.close()
+    conn.close()
+def insert_many(sql, values, host = host, user=user, password=password, database=database):
+    conn = connect(host, user, password, database)
+    cursor = conn.cursor()
+    cursor.executemany(sql, values)
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def get_size_of_support_size(support_suffix, host = host, user=user, password=password, database=database):
+    table_size_list = {}
+    conn = connect(host, user, password, database)
+    cursor = conn.cursor()
+    sql = f"use {database}"
+    cursor.execute(sql)
+    sql = "show tables"
+    cursor.execute(sql)
+    tables = cursor.fetchall()
+    for table in tables:
+        table = str(table[0])
+        if("_all" not in table and support_suffix in table):
+            query = f"SELECT count(*) FROM {database}.{table}"
+            # print(query)
+            cursor.execute(query)
+            table = table.split(support_suffix)[0]
+            table_size_list[table] = cursor.fetchall()[0][0]
+    cursor.close()
+    conn.close()
+    return table_size_list 
+def get_fields_of_all_tables(host = host, user=user, password=password, database=database):
+    table_list = []
+    original_fields = {}
+    table_size_list = {}
+    conn = connect(host, user, password, database)
+    cursor = conn.cursor()
+    sql = f"use {database}"
+    cursor.execute(sql)
+    sql = "show tables"
+    cursor.execute(sql)
+    tables = cursor.fetchall()
+    for table in tables:
+        table = str(table[0])
+        if(table.islower() and "_support" not in table):
+            table_list.append(table)
+            original_fields[table] = []
+            query = f"SELECT count(*) FROM {database}.{table}"
+            # print(query)
+            cursor.execute(query)
+            table_size_list[table] = cursor.fetchall()[0][0]
+            sql = f"desc {database}.{table}"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            for row in result:
+                if(row[0] != 'aID' and row[0] != 'sID'):
+                    original_fields[table].append(row[0])
+    cursor.close()
+    conn.close()
+    return table_list, table_size_list, original_fields
+def get_field_from_table(table, host = host, user=user, password=password, database=database):
+    original_fields = []
+    conn = connect(host, user, password, database)
+    cursor = conn.cursor()
+    query = f"SELECT count(*) FROM {database}.{table}"
+    cursor.execute(query)
+    table_size = cursor.fetchall()[0][0]
+    sql = f"desc {database}.{table}"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    for row in result:
+        if(row[0] != 'aID' and row[0] != 'sID'):
+            original_fields.append(row[0])
+    cursor.close()
+    conn.close()
+    return table_size, original_fields
+
+
+def get_field_domains_from_table(table, host = host, user=user, password=password, database=database):
+    primary_fields = []
+    original_fields = []
+    field_domain = []
+    field_domain_count = []
+    primary_fields_idx = []
+    original_fields_idx = []
+    conn = connect(host, user, password, database)
+    cursor = conn.cursor()
+    query = f"SELECT count(*) FROM {database}.{table}"
+    cursor.execute(query)
+    table_size = cursor.fetchall()[0][0]
+    sql = f"desc {database}.{table}"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    i = 0
+    for row in result:
+        if(row[3] == 'PRI'):
+            primary_fields.append(row[0])
+            primary_fields_idx.append(i)
+        else:
+            original_fields.append(row[0])
+            query = f"SELECT DISTINCT {row[0]} FROM {database}.{table}"
+            cursor.execute(query)
+            field_domain_temp = cursor.fetchall()
+            field_domain.append([field[0] for field in field_domain_temp])
+            field_domain_count.append(len(field_domain_temp))
+            original_fields_idx.append(i)
+        i += 1
+    cursor.close()
+    conn.close()
+
+    return table_size, primary_fields, primary_fields_idx, original_fields, original_fields_idx, field_domain, field_domain_count
+
+
+if __name__ == "__main__":
+    table_list, table_size_list, fields = get_fields_of_all_tables(database="qa_tpch1g")
+    print(table_list)
+    print(table_size_list)
+    print(fields)
+    
+
